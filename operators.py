e2bb146a1a3d90cee5356b635c5805d29d654cb2
Manjri Gupta <manjrigupta452@gmail.com>
2025-11-08 11:22:51 +0530

    updated

---
+++ operators.py
@@ -0,0 +1,30 @@
+x = 10
+y = 5
+
+print(x+y)
+print(x*y)
+print(x%y)
+print(x//y)
+
+a=15
+b=2
+print(a/b)
+print(a//b)
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+fruits = ["apple", "banana", "cherry"]
+
+print("banana" in fruits)
+
+fruits = ["apple", "banana", "cherry"]
+
+print("pineapple" not in fruits)
--- strings.py
+++ strings.py
@@ -8,7 +8,21 @@ When he nothing shines upon,
 Then you show your little light,
 Twinkle, twinkle, all the night.
 """
-print(a)
+#print(a)
 
-a = "Hello, World!"
-print(a[1])
+
+#b="manjri"
+#print(b[ :2])
+
+# c="  shivi manjri gupta  "
+# print(c.upper())
+# print(c.lower())
+# print(c.strip())
+#The strip() methodremoves any whitespace 
+# from the beginning or the end:
+##
+
+a= "Manjri Gupta" #5 place pr jo aayega use count nhi krenge [The first character has index 0].
+print(a[2:5])
+print(a[ :5])#5 place pr jo aayega use count nhi krenge
+print(a[2:])
