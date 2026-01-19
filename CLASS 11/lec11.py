2f803e168667c4f3f846f3934ebe7f0c2fdbcd98
Manjri Gupta <manjrigupta452@gmail.com>
2026-01-19 19:08:03 +0530

    updated

---
+++ CLASS 11/lec11.py
@@ -0,0 +1,27 @@
+# a=int(input("Enter your Age: "))
+# if a>=18:
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")
+
+# x=int(input("Enter your 1st number: "))
+# y=int(input("Enter your 2nd number: "))
+# if x>y:
+#     print("Maximum number is : ",x)
+# else:
+#     print("Maximum number is : ",y)
+
+# x=int(input("Enter your 1st number: "))
+# y=int(input("Enter your 2nd number: "))
+# z=int(input("Enter your 2nd number: "))
+
+# if x>y and x>z:
+#     print("Max number is :",x)
+# elif y>x and y>z:
+#     print("Max number is :",y)
+# else:
+#     print("Max number is :",z)
+
+
+
+
---
+++ CLASS 11/lec12.py
@@ -0,0 +1,8 @@
+# EXCERCISES
+a=int(input("Enter 1st Number: "))
+if a>0:
+    print("Number is Positive ",a)
+elif a<0:
+    print("Number is Negative ",a)
+else:
+    print("Zero")
