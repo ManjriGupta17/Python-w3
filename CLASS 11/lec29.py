ad5b7f2a5042d6ac3faca8b3a7aa09117836ca2c
Manjri Gupta <manjrigupta452@gmail.com>
2026-01-31 21:42:24 +0530

    updated

---
+++ CLASS 11/lec29.py
@@ -0,0 +1,28 @@
+# #Ques1
+# msg=input("Enter welcome message : ")
+# print("Welcome message : ",msg)
+
+##Ques2
+# num1=int(input("Enter 1st Number: "))
+# num2=int(input("Enter 2nd Number: "))
+# if num1>num2:
+#     print("num1 is larger Number",num1)
+# else:
+#     print("num2 is larger number",num2)
+
+##Ques3
+num1=int(input("Enter 1st Number: "))
+num2=int(input("Enter 2nd Number: "))
+num3=int(input("Enter 3rd Number: "))
+
+if num1>num2 and num1>num3:
+    print("num1 is larger Number",num1)
+elif num2>num1 and num2>num3:
+    print("num2 is larger number",num2)
+else:
+    print("num3 is larger number",num3)
+
+#Ques4
+
+
+
